9d80d3847f4d2b4d52cbe358930746aef4825860
Manav <manav@uthaan.ac.in>
2022-04-17 12:46:43 +0530

    bug fix

--- middlewares/dbconnection.js
+++ middlewares/dbconnection.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const mysql = require('mysql2')
-const {createUser, createSession} = require('../controllers/sqlQueries')
+const {createUser, createSession} = require('../controllers/sqlqueries')
 const db_config_user = {
     host: process.env.DB,
     user: process.env.DB_USER,
